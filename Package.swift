47d492eab17ae68323ec50d13430aafd157c9c5b
Geoplan-Mobile <technical.geoplan@gmail.com>
2026-06-10 14:38:28 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "gpi-dltdoa",
-    platforms: [.iOS(.v26)],
     products: [
         .library(name: "gpi-dltdoa", targets: ["gpi-dltdoa"]),
     ],
     targets: [
         .binaryTarget(
             name: "gpi-dltdoa",
-            path: "./gpi-dltdoa.xcframework"
+            path: "gpi-dltdoa.xcframework"
         )
     ]
 )
