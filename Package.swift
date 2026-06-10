2fc8a1dac1286a96b3247a7c6841db502285fa60
Geoplan-Mobile <technical.geoplan@gmail.com>
2026-06-10 14:22:52 +0900

    Update Package.swift
    xcframework path 방식으로 전환

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "gpi-dltdoa",
-            url: "https://github.com/Geoplan-Mobile/gpi-dltdoa/releases/download/1.0.0/gpi-dltdoa.xcframework.zip",
-            checksum: "a7b95c4b9ed3f2ca767af8db5ba14feb1624321f8358cfa69fda48cdbd9a04f3"
+            path: "./gpi-dltdoa.xcframework"
         )
     ]
 )
